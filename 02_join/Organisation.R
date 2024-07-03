d8eaa535c973edc15c4e2d40325f56e4a6c7984d
jorn <j.z.amatdoelrasit@student.vu.nl>
2024-07-03 12:07:19 +0200

    test: test push to branch with first file

---
+++ 02_join/Organisation.R
@@ -0,0 +1,35 @@
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for Education Analytics Vrije Universiteit Amsterdam
+## Copyright 2024 VU
+## Web Page: http://www.vu.nl
+## Contact:
+##
+##' *INFO*:
+## 1) Join SubjectLabel and SubjectToLabel tables.
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+sDir <- paste0(Sys.getenv("RAW_DATA_DIR"), "Evalytics/")
+sMostrecent_zip <- vvmover::get_recent_file_date_modified(sDir, ".zip")
+
+## Read the required CSV files directly from the ZIP file
+dfOrganisation <- readr::read_delim(unz(sMostrecent_zip, "organisation.csv"))
+dfSubject <- readr::read_delim(unz(sMostrecent_zip, "subject.csv"))
+dfTeacher <- readr::read_delim(unz(sMostrecent_zip, "teacher.csv"))
+dfGenericTopic <- readr::read_delim(unz(sMostrecent_zip, "genericTopic.csv"))
+
+## Join the two data frames
+dfOrganisation1 <- dfSubject %>%
+  left_join(dfOrganisation, by = c("organisationId" = "id"))
+
+dfOrganisation2 <- dfTeacher %>%
+  left_join(dfOrganisation, by = c("organisationId" = "id"))
+
+dfOrganisation3 <- dfGenericTopic %>%
+  left_join(dfOrganisation, by = c("organisationId" = "id"))
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## WRITE & CLEAR ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+clear_script_objects()
